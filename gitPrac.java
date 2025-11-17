4136ba92ee61d210cf19ac3c061d46342a9d6d3b
aliyash258 <ashaikh42308@gmail.com>
2025-11-17 13:55:53 -0500

    I created the FirstEven method of part 2.

--- gitPrac.java
+++ gitPrac.java
@@ -1,14 +1,28 @@
-public class gitPrac{
-	public static void main(String[] args){
+public class gitPrac
+{
+	public static void main(String[] args)
+	{
 
 	}
 
-	public void part1(int size){
+	public void part1(int size)
+	{
 		int[][] sqArr = new int[size][size];
 
-		for(int i = 0; i < size; i++){
+		for(int i = 0; i < size; i++)
+		{
 			for(int c = 0; c<size; c++)
 				sqArr[i][c] = (int)(Math.random()*10);
+		}
+	}
+
+	public void FirstEven(int[][] arr)
+	{
+		for(int i=0; i<arr.length; i++)
+		{
+			for(int j=0; j<arr[0].length; j++)
+				if(arr[i][j]%2==0)
+					return arr[i][j];
 		}
 	}
 
